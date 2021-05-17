aa95dc6d432fc9f2d2abb30a5c52052ec4689128
yoojinhong <hongyoojin@hong-yujin-ui-MacBookPro.local>
2021-05-18 01:16:39 +0900

    First commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+    return (
+        <div>
+            <div className="header">
+                <h1 className="header1">Colics</h1>
+                <h3 className="header2">Schedule manager for working moms and dads</h3>
+            </div>
+            <hr className="border"/>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/Components/Menubar/Menubar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Menubar.css'
+
+function Menubar(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
+export default Menubar;
