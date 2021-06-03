bb25fdaf8f6a6038187339984b5b520e7cf2d680
Sang3028 <68941588+Sang3028@users.noreply.github.com>
2021-06-04 01:01:33 +0900

    Before pull

--- src/Components/Projectmenu/ProjectBox/index.jsx
+++ src/Components/Projectmenu/ProjectBox/index.jsx
@@ -5,12 +5,6 @@ const ProjectBox = (props) => {
     
     const {index, project} = props;
 
-    useEffect(() => {
-        var colors = ['#ff0000', '#00ff00', '#0000ff'];
-        var random_color = colors[Math.floor(Math.random() * colors.length)];
-        document.getElementById('proj').style.backgroundColor = random_color;
-      }, []);
-
     return(
         <div className = 'projectmenu-category' id = 'proj'>{project}</div>
     )
